4ee835ea60461e3d4d779a12669e718656e5e789
jsamara <jneenosamara@gmail.com>
2022-06-28 22:25:45 -0500

    Database is all set up to insert data! Let me know if you have any questions! I'll continue to update it hopefully this week!

--- app/src/main/java/com/example/vetapp/MainActivity.java
+++ app/src/main/java/com/example/vetapp/MainActivity.java
@@ -4,33 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    EditText email, password;
+    //Button insert, update, delete, view;
     TextView insert;
-    DBHelper DB = new DBHelper(MainActivity.this);
+    DatabaseReference reff;
+    UserAccount userAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        email = (EditText) findViewById(R.id.txt_login_username);
+        password = (EditText) findViewById(R.id.txt_login_password);
         insert = findViewById(R.id.txt_createAccountLink);
 
         insert.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                String emailTXT = email.getText().toString();
+                String passwordTXT = password.getText().toString();
+                userAccount = new UserAccount();
+                reff = FirebaseDatabase.getInstance().getReference().child("Member");
+                insert.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        userAccount.setEmail(emailTXT);
+                        userAccount.setPassword(passwordTXT);
 
-
-                boolean checkInsertData = DB.insertUserAccount("user@gmail.com", "password");
-                if(checkInsertData){
-                    Toast.makeText(MainActivity.this, "New Entry Inserted", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "New Entry Not Inserted", Toast.LENGTH_SHORT).show();
-                }
+                        reff.push().setValue(userAccount);
+                        Toast.makeText(MainActivity.this, "data inserted successfully", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
