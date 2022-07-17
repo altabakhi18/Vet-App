ade25517644e23a5b10bf7192c536b35f3e188b6
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-07-17 17:41:21 -0500

    Pets Pages
    Added pets pages
    -pets nav
    - appointments
    - prescriptions
    - vaccinations

---
+++ app/src/main/java/com/example/vetapp/petsVax.java
@@ -0,0 +1,14 @@
+package com.example.vetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class petsVax extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pets_vax);
+    }
+}
