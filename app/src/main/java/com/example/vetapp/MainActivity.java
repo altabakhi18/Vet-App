d962078000ad6dbb473a8b4cd368707b790150ac
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-07-25 18:41:46 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/vetapp/MainActivity.java
+++ app/src/main/java/com/example/vetapp/MainActivity.java
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -23,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_login);
         TextView accountCreate = (TextView) findViewById(R.id.txt_createAccountLink);
         accountCreate.setOnClickListener(new View.OnClickListener() {
             @Override
