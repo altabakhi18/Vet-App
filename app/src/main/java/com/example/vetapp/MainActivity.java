af60d957febf01938de60875666bfe95df93919e
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-07-27 19:15:22 -0500

    adding buttons

--- app/src/main/java/com/example/vetapp/MainActivity.java
+++ app/src/main/java/com/example/vetapp/MainActivity.java
@@ -2,35 +2,13 @@ package com.example.vetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText username, password;
-    //Button insert, update, delete, view;
-    TextView create;
-    DatabaseReference reff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_login);
-        TextView accountCreate = (TextView) findViewById(R.id.txt_createAccountLink);
-        accountCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, createaccount.class));
-            }
-        });
-
-        username = (EditText) findViewById(R.id.txt_login_username);
-        password = (EditText) findViewById(R.id.txt_login_password);
+        setContentView(R.layout.activity_main);
     }
 }
