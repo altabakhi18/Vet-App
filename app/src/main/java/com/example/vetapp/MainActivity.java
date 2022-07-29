5d12e00f5957cebcea55a8ed87654cfd4719ef5e
jsamara <jneenosamara@gmail.com>
2022-07-29 13:58:20 -0500

    Linked the pages and put a little bit of data into the appointment history page.

--- app/src/main/java/com/example/vetapp/MainActivity.java
+++ app/src/main/java/com/example/vetapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.vetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.btn_pets);
+        Button button2 = (Button) findViewById(R.id.btn_appts);
+        Button button3 = (Button) findViewById(R.id.btn_emergency);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btn_pets:
+                startActivity(new Intent(MainActivity.this, petsHomeScreen.class));
+                break;
+            case R.id.btn_appts:
+                startActivity(new Intent(MainActivity.this, appts.class));
+                break;
+            case R.id.btn_emergency:
+                startActivity(new Intent(MainActivity.this, emergency.class));
+                break;
+        }
     }
 }
