a4a3430c1898973ca9e1157156ceaa37f67c91ae
altabakhi18 <107474849+altabakhi18@users.noreply.github.com>
2022-07-31 16:04:03 -0500

    initial commit

--- app/src/main/java/com/example/vetapp/appt_request.java
+++ app/src/main/java/com/example/vetapp/appt_request.java
@@ -1,15 +1,25 @@
 package com.example.vetapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class appt_request extends AppCompatActivity {
-
+        AppCompatButton submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appt_request);
+        submit = findViewById(R.id.aptReq_btn_submit);
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(appt_request.this, "Thank you for your request!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vetapp/emergency.java
+++ app/src/main/java/com/example/vetapp/emergency.java
@@ -1,14 +1,30 @@
 package com.example.vetapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class emergency extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
+
+        button = findViewById(R.id.btn_emergency_home);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+            }
+        });
+    }
+    public void openHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
