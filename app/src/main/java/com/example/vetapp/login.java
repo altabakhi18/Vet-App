898d2662234e08c3b94ab004df7abf282f433b8d
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-08-07 17:59:37 -0500

    Update login.java

--- app/src/main/java/com/example/vetapp/login.java
+++ app/src/main/java/com/example/vetapp/login.java
@@ -8,8 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.widget.Toast;
+
 
 public class login extends AppCompatActivity implements View.OnClickListener{
 
